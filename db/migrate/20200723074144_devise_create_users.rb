13b4c9ead93219e354f63df5c90bce1bc95836ce
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-07-23 17:15:31 +0900

    Update users_table
    deviseを用いたusersテーブルのマイグレーションファイルを編集してカラムを追加しました

--- db/migrate/20200723074144_devise_create_users.rb
+++ db/migrate/20200723074144_devise_create_users.rb
@@ -3,8 +3,15 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :nickname,         null: false
+      t.string :first_name,       null: false
+      t.string :last_name,        null: false
+      t.string :first_name_kana,  null: false
+      t.string :last_name_kana,   null: false
+      t.date   :birthday,         null: false
+
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
