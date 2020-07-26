fa49068b77c4e9ae9577272ca8d73c30b36e4cab
kamoto0427 <takahiro.08039@gmail.com>
2020-07-26 17:53:49 +0900

    クレジットカード登録画面の作成
    credit_cards/newのurlのページに、クレジットカードの登録画面を作成。

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :user, only: [:show]
   resources :credit_cards, only: [:index, :new, :show] do
-    collection do
+    member do
       post 'pay', to: 'credit_cards#pay'
       post 'delete', to: 'credit_cards#delete'
       post 'show', to: 'credit_cards#show'
