ab1369e9e4d7bf850606f9f438c2d41cc9665ace
kamoto0427 <takahiro.08039@gmail.com>
2020-07-26 17:39:37 +0900

    credit_cardsコントローラー/ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'credit_cards/index'
+  get 'credit_cards/new'
+  get 'credit_cards/show'
   devise_for :users
   root 'items#index'
   resources :items, only: [:index]
   resources :user, only: [:show]
+  resources :credit_cards, only: [:index, :new, :show] do
+    collection do
+      post 'pay', to: 'credit_cards#pay'
+      post 'delete', to: 'credit_cards#delete'
+      post 'show', to: 'credit_cards#show'
+    end
+  end
 end
---
+++ test/controllers/credit_cards_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CreditCardsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get credit_cards_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get credit_cards_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get credit_cards_show_url
+    assert_response :success
+  end
+
+end
