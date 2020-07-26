825918059d67523ddbbb91d3641da2f2dff355d0
kamoto0427 <takahiro.08039@gmail.com>
2020-07-26 19:06:39 +0900

    クレジットカード一覧画面の作成
    クレジットカードの一覧画面の作成とクレジットカードを登録する一連のページ(一覧表示、登録画面、クレジット追加画面)の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'credit_cards/index'
-  get 'credit_cards/new'
-  get 'credit_cards/show'
   devise_for :users
   root 'items#index'
   resources :items, only: [:index]
   resources :user, only: [:show]
   resources :credit_cards, only: [:index, :new, :show] do
-    member do
+    collection do
       post 'pay', to: 'credit_cards#pay'
       post 'delete', to: 'credit_cards#delete'
       post 'show', to: 'credit_cards#show'
