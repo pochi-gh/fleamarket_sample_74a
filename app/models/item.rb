830cfe9ad8a821502394d18d48200da5248f0572
kamoto0427 <takahiro.08039@gmail.com>
2020-08-03 22:16:54 +0900

    アソシエーション/データベースの商品名,価格が表示されるようにした

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  has_many :images, dependent: :destroy
   belongs_to_active_hash :state
   belongs_to_active_hash :shipping_burden
 end
