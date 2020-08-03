06bcbf3f1e73fe7e2022ef0692c62d747c9875ae
kamoto0427 <takahiro.08039@gmail.com>
2020-08-03 21:07:24 +0900

    shipping_burden.rbの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-
   def index
     @items = Item.all
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :state
   belongs_to_active_hash :shipping_burden
 end
