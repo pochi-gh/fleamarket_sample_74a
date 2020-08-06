8b39c888c3bcda830eab394f8041a0f531676167
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-08-06 16:16:31 +0900

    modified validates in model/user.rb
    正規表現を用いて、メールアドレスや名前の入力を指定した文字で入力しないといけないようにしました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+  validates :nickname,
+    presence: true
+  validates :email,
+    format: { with: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/},
+    presence: true
+  validates :first_name,
+    format: { with: /^[ぁ-んァ-ン一-龥]/},
+    presence: true
+  validates :last_name,
+    format: { with: /^[ぁ-んァ-ン一-龥]/},
+    presence: true
+  validates :first_name_kana,
+    format: { with: /^([ァ-ン]|ー)+$/},
+    presence: true
+  validates :last_name_kana,
+    format: { with: /^([ァ-ン]|ー)+$/},
+    presence: true
+  validates :birthday,
+    presence: true
   has_one :address
 end
