1217fc1853554ef24442faa210186cb1da03cfd9
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-08-06 15:50:17 +0900

    Preparation for rspec
    factory_botを利用する準備をしました

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"toda"}
+    email                 {"todatoda@mail.com"}
+    password              {"12345678"}
+    password_confirmation {"12345678"}
+  end
+
+end
