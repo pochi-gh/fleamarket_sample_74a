8de8da120d2945d2022dee4993db74faf7f1d1b7
pochi-gh <pochigm@gmail.com>
2020-08-12 12:53:44 +0900

    imageテーブル　mgファイル　カラム記述追加

--- db/migrate/20200812033751_create_images.rb
+++ db/migrate/20200812033751_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-
+      t.references :item, foreign_key: true
+      t.string :photo
       t.timestamps
     end
   end
