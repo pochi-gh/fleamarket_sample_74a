035f199d5cc08a97243ff52f83e002ec116d2406
pochi-gh <pochigm@gmail.com>
2020-08-12 12:25:15 +0900

    Revert "モデル作成"
    This reverts commit b36248220385e35d00b5fc6344e96b30aa1cc3cf.

--- app/models/state.rb
+++
@@ -1,6 +0,0 @@
-class State < ActiveHash::Base
-  self.data = [
-    {id: 1, name: '新品、未使用'}, {id: 2, name: '未使用に近い'}, {id: 3, name: 'やや傷や汚れあり'},
-    {id: 4, name: '傷や汚れあり'}, {id: 5, name: '全体的に状態が悪い'}
-  ]
-end
