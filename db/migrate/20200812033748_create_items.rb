8849d6752341b9f8a40a9d4789a529282386ce6e
pochi-gh <pochigm@gmail.com>
2020-08-12 12:51:24 +0900

    items　マイグレーションファイルカラム記述

--- db/migrate/20200812033748_create_items.rb
+++ db/migrate/20200812033748_create_items.rb
@@ -1,7 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false #名前
+      t.text :explain, null: false #商品説明
+      t.references :category, null: false #カテゴリー カテゴリー機能追加後、外部キー設定要
+      t.text :brand #ブランド名
+      t.references :state, null: false #商品状態
+      t.references :shipping_burden, null: false #送料負担
+      t.references :reservation_email, null: false #発送方法
+      t.references :prefecture, null: false #発送地域
+      t.references :shipping_day, null: false #発送までの日数
+      t.integer :price, null: false #商品価格
+      t.references :seller, null: false, foreign_key: { to_table: :users } #売主
+      t.references :buyer, foreign_key: { to_table: :users } #買主
       t.timestamps
     end
   end
