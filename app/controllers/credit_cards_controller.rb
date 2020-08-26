cf74b2d3c052c35a942f967ccf7307cbaec6ce6a
kamoto0427 <takahiro.08039@gmail.com>
2020-08-26 15:49:48 +0900

    credit_cardコントローラーに記述
    クレジットカードを登録するためのコントローラーを記述。
    
    実装内容は、カードの登録とトークン発行、カードの詳細。
    
    カードの削除機能は一旦ストップ。

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,10 +1,42 @@
 class CreditCardsController < ApplicationController
-  def index
-  end
+
+  require "payjp"
 
   def new
+    card = CreditCard.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
   end
 
   def show
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+      logger.debug("@default_card_information")
+    end
+  end
+
+  def delete
+  end
+
+  def pay
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
   end
 end
