3039cfb2dd2ff9dc1ab942fe0b495185f7e96f4c
pochi-gh <pochigm@gmail.com>
2020-08-30 18:41:39 +0900

    purechase 実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname 
   end
-  
+  def purchase
+    @items = Item.where(buyer_id: current_user.id).page(params[:page]).per(10)
+    @images =Image.includes(:user)
+  end
 end
