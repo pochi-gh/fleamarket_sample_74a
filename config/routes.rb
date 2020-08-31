80da59bc3e54a4dcfda6f10ee055753c027b429f
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-08-31 20:36:32 +0900

    update routes.rb
    Ajaxに対応したルーティングにしました

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   end
   root 'items#index'
   resources :items do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
     member do
       get 'confirm'
-      get 'sell_confirm', to: 'items/sell_comfirm'
     end
   end
   resources :users, only: [:show] do
