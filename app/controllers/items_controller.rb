4fd75dbf48803266ae37187a42948bed9b5ec24e
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-09-01 21:20:13 +0900

    update items_controller.rb
    before_actionのexceptに子カテゴリや孫カテゴリを取得するメソッドを追加しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: [:index, :new, :create]
+  before_action :set_item, except: [:index, :new, :create, :get_category_children, :get_category_grandchildren]
 
   def index
   end
