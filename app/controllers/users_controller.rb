a4d31c6658f74eeb370d0ed2791e5f6e831dac73
pochi-gh <pochigm@gmail.com>
2020-09-03 18:38:10 +0900

    @imagesの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,10 @@ class UsersController < ApplicationController
   end
   def purchase_item
     @items = Item.where(buyer_id: current_user.id).page(params[:page]).per(10)
-    @images =Image.includes(:user)
   end
 
   def sell_item
     @items = Item.where(seller_id: current_user.id).page(params[:page]).per(10)
-    @images =Image.includes(:user)
   end
 
 end
