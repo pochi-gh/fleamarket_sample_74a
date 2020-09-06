286b5edfce9532e94d9065416b87c6da411160ad
pochi-gh <pochigm@gmail.com>
2020-09-06 15:16:16 +0900

    テキストカウントをコピペに対応

--- app/assets/javascripts/text_count.js
+++ app/assets/javascripts/text_count.js
@@ -1,5 +1,6 @@
 $(function(){
-  $("#item_explain").keyup(function(){
+  $("#item_explain").on('change keyup',function(){
+
     let txtcount = $(this).val().length;
     console.log(txtcount)
     $(".items--count").text(txtcount);
