e9ae0b00b835427b277ed938f2b5329e05c1d584
pochi-gh <pochigm@gmail.com>
2020-09-07 18:25:38 +0900

    クレジットカードがない際に購入ボタンの非表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,16 +41,16 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @seller = User.find(@item.seller_id)
-    @images = @item.images
     @comment = Comment.new
     @items = Item.where(category: @item.category_id).where.not(id: @item.id).order(created_at: :desc).limit(3)
-    @comments = @item.comments
   end
 
   def confirm
     @item = Item.find(params[:id])
     @image = Image.find(params[:id])
-    @address = Address.where(user_id: current_user.id).first
+    @user = User.find(current_user.id)
+    @card = CreditCard.find_by(user_id: current_user.id)
+    binding.pry
   end
 
   def get_category_children
