ea5b40c8b25604983bbbf5ab8f6e9346849e4c18
kamoto0427 <takahiro.08039@gmail.com>
2020-09-07 23:52:29 +0900

    商品一覧の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   before_action :set_category, only: [:parent, :child, :grandchild]
 
   def index
+    @items = Item.all
     @categories = Category.all  
   end
   
