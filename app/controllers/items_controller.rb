bfa261f673c65e7ee43205bb873b59a2dc056b09
pochi-gh <pochigm@gmail.com>
2020-09-08 13:40:54 +0900

    editアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,27 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+  end
+
+
   def show
     @item = Item.find(params[:id])
     @seller = User.find(@item.seller_id)
