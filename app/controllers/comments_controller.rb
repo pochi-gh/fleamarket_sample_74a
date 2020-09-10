596e7108ad607e6ed86356355bf3e886397cb5f4
pochi-gh <pochigm@gmail.com>
2020-09-10 11:38:30 +0900

    コメント周りのエラーハンドリング追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,37 @@ class CommentsController < ApplicationController
   before_action :set_categorys
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to item_path(comment.item.id)
+    comment = Comment.new(comment_params)
+    if comment.valid?
+      comment.save
+      flash[:notice] = 'コメントしました。'
+      redirect_to item_path(comment.item.id)
+    else
+      flash.now[:alert] = 'コメントを入力してください'
+      @item = Item.find(params[:item_id])
+      @seller = User.find(@item.seller_id)
+      @comment = Comment.new
+      @items = Item.where(category: @item.category_id).where.not(id: @item.id).order(created_at: :desc).limit(3)
+      render template: 'items/show'
+    end
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(comment_params)
+      flash[:notice] = '更新が完了しました'
+      redirect_to item_path(params[:item_id])
+    else
+      @item = Item.find(params[:item_id])
+      @comment = Comment.find(params[:id])
+      flash.now[:alert] = '更新できませんでした。コメントを入力してください。'
+      render :edit
+    end
   end
 
   def edit
     @item = Item.find(params[:item_id])
     @comment = Comment.find(params[:id])
-  end
-
-  def update
-    comment = Comment.find(params[:id])
-    comment.update(comment_params)
-    redirect_to item_path(params[:item_id])
   end
 
   def destroy
