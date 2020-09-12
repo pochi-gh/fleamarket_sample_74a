0b51fafc2014c26f69808357e4976958a437b928
kamoto0427 <takahiro.08039@gmail.com>
2020-09-12 14:54:32 +0900

    本番環境でbuyerの不具合発生のため

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
   has_many :images, dependent: :destroy
   belongs_to :category
   has_many :comments, dependent: :destroy
