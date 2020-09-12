604dd154ebad17a02ebd44a6e505db8e2218e299
kamoto0427 <takahiro.08039@gmail.com>
2020-09-12 20:52:32 +0900

    マイページのパンくず

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,6 +6,21 @@ crumb :mypage do
   link "#{current_user.nickname}のマイページ", user_path(current_user.id), method: :get
 end
 
+crumb :mypage_card do
+  link "クレジットカード", controller: 'credit_cards'
+  parent :mypage
+end
+
+crumb :mypage_personal_information do
+  link "登録されている個人情報", personal_infomation_user_path(current_user.id)
+  parent :mypage
+end
+
+crumb :mypage_logout do
+  link "ログアウト", logout_user_path(current_user.id)
+  parent :mypage
+end
+
 # crumb :projects do
 #   link "Projects", projects_path
 # end
