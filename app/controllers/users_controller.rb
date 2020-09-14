3b572d5cfa5183c12808cf608265236248244f2f
pochi-gh <pochigm@gmail.com>
2020-09-15 02:56:39 +0900

    マイページ周りの未実装部分のコメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @nickname = current_user.nickname
+    @items = Item.where(seller_id: current_user.id) 
   end
 
   def edit
@@ -15,15 +16,15 @@ class UsersController < ApplicationController
   end
 
   def purchase_item
-    @items = Item.where(buyer_id: current_user.id).page(params[:page]).per(10)
+    @items = Item.where(buyer_id: current_user.id).page(params[:page]).per(7)
   end
 
   def sell_item
-    @items = Item.where(seller_id: current_user.id).page(params[:page]).per(10)
+    @items = Item.where(seller_id: current_user.id).page(params[:page]).per(7)
   end
 
   def comment_item
-    @comments = Comment.where(user_id: current_user.id).select(:item_id).distinct.page(params[:page]).per(10)
+    @comments = Comment.where(user_id: current_user.id).select(:item_id).distinct.page(params[:page]).per(7)
   end
 
 
