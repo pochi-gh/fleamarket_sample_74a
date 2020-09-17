fb197775e52adfbde7f7e40dea3641302d799458
Takamie-Kai <64832764+Takamie-Kai@users.noreply.github.com>
2020-09-17 18:55:54 +0900

    カタカナバリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,13 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates  :destination_first_name, :destination_last_name, :destination_first_name_kana, :destination_last_name_kana, :postal_code, :prefecture_id, :city, :block, presence: true
   
+  validates :destination_first_name_kana,
+  format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/},
+  presence: true
+  validates :destination_last_name_kana,
+  format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/},
+  presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
